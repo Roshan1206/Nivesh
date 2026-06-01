277617184df4f299b7e2891a4b10eff10be17da3
Roshan <sahuroshan96@gmail.com>
2026-06-01 15:27:15 +0530

    excluded auto configs from library

--- gateway/src/main/java/com/nivesh/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/nivesh/gateway/GatewayApplication.java
@@ -1,9 +1,10 @@
 package com.nivesh.gateway;
 
+import com.nivesh.library.NiveshLibraryAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = NiveshLibraryAutoConfiguration.class)
 public class GatewayApplication {
 
 	public static void main(String[] args) {
